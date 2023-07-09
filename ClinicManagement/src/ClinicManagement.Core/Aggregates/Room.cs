c3b1bd8901c7227defbfd3bc6b66b53b060bc274
Vitor Villar <vsilva18@slb.com>
2023-07-09 08:24:51 -0500

    merge content from repos

---
+++ ClinicManagement/src/ClinicManagement.Core/Aggregates/Room.cs
@@ -0,0 +1,23 @@
+﻿using PluralsightDdd.SharedKernel;
+using PluralsightDdd.SharedKernel.Interfaces;
+
+namespace ClinicManagement.Core.Aggregates
+{
+  public class Room : BaseEntity<int>, IAggregateRoot
+  {
+    public string Name { get; set; }
+
+    private Room() { }
+    
+    public Room(int id, string name)
+    {
+      Id = id;
+      Name = name;
+    }
+
+    public override string ToString()
+    {
+      return Name;
+    }
+  }
+}
